4757e8ed92287634c41ddfacc424fa7383e453f9
mogita <me@mogita.com>
2023-09-07 06:42:15 +0000

    feat: osm tag filter

--- gui/src/pages/josm-validation-converter/index.jsx
+++ gui/src/pages/josm-validation-converter/index.jsx
@@ -1,27 +1,29 @@
 import { useState } from 'react'
 import { os } from '@neutralinojs/lib'
-import { Box, Button, Flex, Spacer, Text } from '@chakra-ui/react'
-import { TiArrowRepeatOutline } from 'react-icons/ti'
-import { dirname, getLastOpenedDir, setLastOpenedDir } from './../../utils/fs'
+import { Box, Button, Divider, Flex, Heading, Text } from '@chakra-ui/react'
+import { BsFillPlayCircleFill } from 'react-icons/bs'
+
+import { getLastOpenedDir, setLastOpenedDir } from './../../utils/fs'
 import { getCommandPath } from './../../utils/cmd'
+import path from '../../utils/path'
 
 export default function JOSMValidationConverter() {
   const [xmlPath, setXmlPath] = useState('')
   const [csvPath, setCSVPath] = useState('')
   const [cmdRunning, setCmdRunning] = useState(false)
 
-  const convert = async () => {
+  const start = async () => {
     if (cmdRunning) {
       return
     }
     try {
       setCmdRunning(true)
       const cmd = await getCommandPath('glancet')
-      const fullCmd = `${cmd} convert-josm-validation --xml "${xmlPath}" --csv "${csvPath}"`
-      await os.spawnProcess(`echo "🤖 ${fullCmd}"`)
+      const fullCmd = `"${cmd}" convert-josm-validation --xml "${xmlPath}" --csv "${csvPath}"`
+      await os.spawnProcess(`echo '🤖 ${fullCmd}'`)
       const result = await os.execCommand(fullCmd)
-      await os.spawnProcess(`echo "${result.stdOut}"`)
-      await os.spawnProcess(`echo "${result.stdErr}"`)
+      await os.spawnProcess(`echo '${result.stdOut}'`)
+      await os.spawnProcess(`echo '${result.stdErr}'`)
     } catch (err) {
       console.error(err)
       os.showMessageBox('OsmFlux', err.toString(), 'OK', 'ERROR')
@@ -33,13 +35,13 @@ export default function JOSMValidationConverter() {
   const openXML = async () => {
     try {
       const entries = await os.showOpenDialog('Choose XML', {
-        defaultPath: getLastOpenedDir(),
+        defaultPath: await getLastOpenedDir(),
         multiSelections: false,
         filters: [{ name: 'XML Files', extensions: ['xml'] }],
       })
       if (Array.isArray(entries) && entries.length > 0) {
         setXmlPath(entries[0])
-        setLastOpenedDir(dirname(entries[0]))
+        setLastOpenedDir(path.dirname(entries[0]))
       }
     } catch (err) {
       console.error(err)
@@ -50,13 +52,13 @@ export default function JOSMValidationConverter() {
   const saveCSV = async () => {
     try {
       const filename = await os.showSaveDialog('Save CSV As', {
-        defaultPath: getLastOpenedDir(),
+        defaultPath: await getLastOpenedDir(),
         multiSelections: false,
         filters: [{ name: 'CSV Files', extensions: ['csv'] }],
       })
       if (filename) {
         setCSVPath(filename)
-        setLastOpenedDir(dirname(filename))
+        setLastOpenedDir(path.dirname(filename))
       }
     } catch (err) {
       console.error(err)
@@ -65,22 +67,35 @@ export default function JOSMValidationConverter() {
   }
 
   return (
-    <Flex direction='column' p={2} justifyContent='space-between' h='100%' w='100%'>
+    <Flex direction='column' justifyContent='space-between' h='100%' w='100%'>
+      <Flex alignItems='center' mb={3}>
+        <Text color='#666565' fontSize='sm' fontWeight='bold' mr={4}>
+          Parameters
+        </Text>
+        <Divider />
+      </Flex>
+
       <Flex w='100%' mb={2} justifyContent='center' alignItems='center' alignContent='center'>
         <Box w='50%'>
+          <Heading size='xs' mb={2}>
+            Source XML
+          </Heading>
           <Button onClick={openXML} size='xs' colorScheme='telegram'>
-            Open XML ...
+            Open XML...
           </Button>
-          <Text mt={1} fontSize='sm'>
+          <Text mt={1} color='#666565' fontSize='sm'>
             {xmlPath || 'Please choose an XML file'}
           </Text>
         </Box>
 
         <Box w='50%'>
+          <Heading size='xs' mb={2}>
+            Target CSV
+          </Heading>
           <Button onClick={saveCSV} size='xs' colorScheme='telegram'>
-            Save CSV As ...
+            Save CSV As...
           </Button>
-          <Text mt={1} fontSize='sm'>
+          <Text mt={1} color='#666565' fontSize='sm'>
             {csvPath || 'Please specify where to save the csv file'}
           </Text>
         </Box>
@@ -90,15 +105,15 @@ export default function JOSMValidationConverter() {
 
       <Flex px={4} py={4} mt={8} justifyContent='center' alignItems='center' alignContent='center'>
         <Button
-          leftIcon={<TiArrowRepeatOutline />}
+          leftIcon={<BsFillPlayCircleFill />}
           colorScheme='telegram'
           size='sm'
           minW='30%'
-          onClick={convert}
+          onClick={start}
           isDisabled={!(xmlPath && csvPath)}
           isLoading={cmdRunning}
         >
-          Convert
+          Start
         </Button>
       </Flex>
     </Flex>
